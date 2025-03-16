e36c55896bb602f5eecb9ac5d34bfc2287c5c625
rutujatech <rutup1701@gmail.com>
2025-03-16 10:46:07 +0530

    Addition of two numbers

---
+++ pro1.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+
+int a,b;
+
+printf("enter the value a");
+scanf("%d",&a);
+
+
+printf("enter the value b");
+scanf("%d",&b);
+
+
+int c=a+b;
+printf("addition of a+b =%d" ,c);
+
+
+}
