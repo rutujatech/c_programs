17ac9523de0aedbdc9005b30b9af7b7bd6579af7
rutujatech <rutup1701@gmail.com>
2025-03-20 09:24:56 +0530

    short hand operator

---
+++ pro7.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+
+int a; printf("enter the value of a");
+scanf("%d",&a);
+
+printf("before division value of a%d\n",a);
+//a=a/10;
+
+a/=10;
+printf("after division value of a=%d",a);
+
+}
