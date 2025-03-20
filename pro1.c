5fbecb7e9b58dfe01e011f53e16267e459853ca3
rutujatech <rutup1701@gmail.com>
2025-03-20 09:21:48 +0530

    minicalculater

--- pro1.c
+++ pro1.c
@@ -2,18 +2,23 @@
 void main()
 {
 
-int a,b;
+int a,b,c,d,e,f,g;
 
 printf("enter the value a");
 scanf("%d",&a);
 
-
 printf("enter the value b");
 scanf("%d",&b);
 
+c=a+b;
+d=a-b;
+e=a*b;
+f=a/b;
+g=a%b;
 
-int c=a+b;
-printf("addition of a+b =%d" ,c);
-
-
+printf("addition of a+b=%d\n",c);
+printf("substraction of a-b=%d\n",d);
+printf("multliplication of a*b=%d\n",e);
+printf("division of a/b=%d\n",f);
+printf("reminder of a%b=%d",g);
 }
